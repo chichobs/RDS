79bbf399f0bfb524775b6ad3a5336f6c1b2a7d59
chichobs <chichobs206@gmail.com>
2025-03-12 22:15:48 -0400

    listo

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ document.querySelector('.barra-busqueda').addEventListener('input', function() {
     const searchTerm = this.value.toLowerCase();
     const links = document.querySelectorAll('.link a');
 
-    links.forEach(link => {
-        if (link.textContent.toLowerCase().includes(searchTerm)) {
-            link.classList.add('highlight');
-        } else {
+    if (searchTerm === '') {
+        links.forEach(link => {
             link.classList.remove('highlight');
-        }
-    });
+        });
+    } else {
+        links.forEach(link => {
+            if (link.textContent.toLowerCase().includes(searchTerm)) {
+                link.classList.add('highlight');
+            } else {
+                link.classList.remove('highlight');
+            }
+        });
+    }
 });
